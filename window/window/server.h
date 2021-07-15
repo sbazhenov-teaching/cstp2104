1d7828f98e7d8b57cee4c4f119704297dc03db0f
Sergei Bazhenov <bazhenovwins@gmail.com>
2021-07-14 17:00:44 -0700

    Nonblocking accept

--- window/window/server.h
+++ window/window/server.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <networkLib/winSockInit.h>
 #include <networkLib/socket.h>
+#include <windowLib/window.h>
 
 namespace ServerApp
 {
@@ -8,12 +9,22 @@ namespace ServerApp
 class Server
 {
 public:
+    Server(HINSTANCE hInstance);
+    ~Server();
     void init();
     Network::Socket acceptClient();
     void serve();
+    void stop();
 private:
     Network::WinSock mWinSock;
     Network::Socket mListenSocket;
+    //Window mWindow;
+    HANDLE mStopEvent;
+    WSAEVENT mListenEvent;
+
+    void onCreate(Window&);
+    LRESULT processMessage(
+        HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 };
 
 }
