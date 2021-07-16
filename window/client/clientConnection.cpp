d3f0a9d611d02b3862cb6bc0bd58a2bef283b1f2
Sergei Bazhenov <bazhenovwins@gmail.com>
2021-07-15 17:18:15 -0700

    Improve client

--- window/client/clientConnection.cpp
+++ window/client/clientConnection.cpp
@@ -1,22 +1,31 @@
 #include "clientConnection.h"
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <networkLib/winSockInit.h>
-#include <networkLib/socket.h>
 #include <networkLib/addrInfo.h>
+#include <assert.h>
 
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "27015"
 
-void sendToServer()
+void ClientConnection::sendToServer()
 {
     const char* sendbuf = "this is a test";
-    char recvbuf[DEFAULT_BUFLEN];
-    int iResult;
-    int recvbuflen = DEFAULT_BUFLEN;
+    int iResult{ ::send(mConnectSocket, sendbuf, (int)strlen(sendbuf), 0) };
+    assert(iResult != SOCKET_ERROR);
+}
 
-    Network::WinSock winSock;
-    Network::Socket connectSocket;
+void ClientConnection::shutdown()
+{
+    int iResult{ ::shutdown(mConnectSocket, SD_SEND) };
+    assert(iResult != SOCKET_ERROR);
+}
+
+void ClientConnection::init()
+{
+    //char recvbuf[DEFAULT_BUFLEN];
+    int iResult;
+    //int recvbuflen = DEFAULT_BUFLEN;
+
     {
         addrinfo hints;
         ZeroMemory(&hints, sizeof(hints));
@@ -30,39 +39,31 @@ void sendToServer()
         for (addrinfo* ptr{ addrInfo }; ptr != NULL; ptr = ptr->ai_next) {
 
             // Create a SOCKET for connecting to server
-            connectSocket = Network::Socket(ptr->ai_family, ptr->ai_socktype,
+            mConnectSocket = Network::Socket(ptr->ai_family, ptr->ai_socktype,
                 ptr->ai_protocol);
-            if (!connectSocket.isValid()) {
+            if (!mConnectSocket.isValid()) {
                 return;
             }
 
             // Connect to server.
-            iResult = ::connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+            iResult = ::connect(mConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
             if (iResult == SOCKET_ERROR) {
-                connectSocket = Network::Socket();
+                mConnectSocket = Network::Socket();
                 continue;
             }
             break;
         }
     }
 
-    if (!connectSocket.isValid()) {
-        return;
-    }
+    assert(mConnectSocket.isValid());
 
     // Send an initial buffer
-    iResult = ::send(connectSocket, sendbuf, (int)strlen(sendbuf), 0);
-    if (iResult == SOCKET_ERROR) {
-        return;
-    }
+
 
     //printf("Bytes Sent: %ld\n", iResult);
 
     // shutdown the connection since no more data will be sent
-    iResult = ::shutdown(connectSocket, SD_SEND);
-    if (iResult == SOCKET_ERROR) {
-        return;
-    }
+
 
     //// Receive until the peer closes the connection
     //do {
--- window/client/clientConnection.h
+++ window/client/clientConnection.h
@@ -1,3 +1,16 @@
 #pragma once
+#include <networkLib/winSockInit.h>
+#include <networkLib/socket.h>
 
-void sendToServer();
+class ClientConnection
+{
+public:
+    void init();
+    void sendToServer();
+    void shutdown();
+private:
+    Network::WinSock mWinSock;
+    Network::Socket mConnectSocket;
+};
+
+
